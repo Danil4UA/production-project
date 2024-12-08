53a0d72cdbd441dc61b5ebd850f511bbdd9232f5
Danil4UA <146213395+Danil4UA@users.noreply.github.com>
2024-12-08 20:00:33 +0200

    implemented redux-toolkit, add counter component and tests for slice, reducer

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,26 +4,16 @@ import { AppRouter } from "./providers/router";
 import { Navbar } from "widgets/Navbar";
 import { Sidebar } from "widgets/Sidebar";
 import { Suspense } from "react";
-import Modal from "shared/ui/Modal/Modal";
-import { useState } from "react";
 
 const App = () => {
   
     const { theme } = useTheme()
 
-    const [isOpen, setIsOpen] = useState(false)
-
-    
-
     return (
         <>
             <div className={classNames("app", {}, [theme])}>   
                 <Suspense fallback="">
-                    <Navbar />
-                    <button onClick={()=>setIsOpen(true)}>{"toggle"}</button>
-                    <Modal isOpen={isOpen} onClose={()=>setIsOpen(false)} >
-                        {"Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti incidunt eum unde laborum amet explicabo alias quae dolore nobis ullam?"}
-                    </Modal>
+                    <Navbar />              
                     <div className="content-page">
                         <Sidebar />
                         <AppRouter />
---
+++ src/entities/Counter/model/selectors/getCounterValue/getCounterValue.test.ts
@@ -0,0 +1,13 @@
+/* eslint-disable no-undef */
+import { DeepPartial } from "@reduxjs/toolkit"
+import { getCounterValue } from "./getCounterValue"
+import { StateSchema } from "app/providers/StoreProvider"
+
+describe("getCounterValue.test", ()=>{
+    test("", ()=> {
+        const state: DeepPartial<StateSchema> = {
+            counter: {value: 10},
+        }
+        expect(getCounterValue(state as StateSchema)).toEqual(10)
+    })
+})
